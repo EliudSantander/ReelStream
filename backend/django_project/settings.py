355e938a42c6f34a224c1706fa73edb98ec5dc59
Eliud Santander <eliud.santander@gmail.com>
2026-02-16 13:57:34 -0700

    set global pagination

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     "catalog",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 100,
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
