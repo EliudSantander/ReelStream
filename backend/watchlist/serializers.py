47e4dc4c8b88aea527d3fc3770b70bd32a31f8b2
Eliud Santander <eliud.santander@gmail.com>
2026-02-20 18:23:43 -0700

    update watchlist serializer to auto set user and not bring that field

--- backend/watchlist/serializers.py
+++ backend/watchlist/serializers.py
@@ -1,27 +1,29 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
+from catalog.models import Film
 from .models import Watchlist
 from catalog.serializers import FilmSerializer
 
 
 class WatchlistSerializer(serializers.ModelSerializer):
     film = FilmSerializer(read_only=True)
-    film_id = serializers.IntegerField(write_only=True)
+    film_id = serializers.PrimaryKeyRelatedField(
+        queryset=Film.objects.all(),
+        source="film",
+        write_only=True,
+    )
 
     class Meta:
         model = Watchlist
-        fields = "__all__"
-
-    def create(self, validated_data):
-        film_id = validated_data.pop("film_id")
-        return Watchlist.objects.create(film_id=film_id, **validated_data)
+        fields = ["id", "film", "film_id", "added_at"]
+        read_only_fields = ["user", "added_at"]
 
     def validate(self, attrs):
         user = self.context["request"].user
-        film_id = attrs.get("film_id")
+        film = attrs.get("film")
 
-        if Watchlist.objects.filter(user=user, film_id=film_id).exists():
+        if Watchlist.objects.filter(user=user, film=film).exists():
             raise ValidationError({"detail": "This film is already in your watchlist."})
 
         return attrs
