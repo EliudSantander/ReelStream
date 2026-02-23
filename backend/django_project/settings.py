dce510e316d6b4bef44e7f86b4952755aab30a2e
Eliud Santander <eliud.santander@gmail.com>
2026-02-22 22:47:37 -0700

    adjust urls

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -49,6 +49,8 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
+APPEND_SLASH = True
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
