a4d435df08033cd524f6d4f75d7368feebfb47d3
Eliud Santander <eliud.santander@gmail.com>
2026-06-10 16:18:56 -0700

    catalog views refactored

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,18 +1,11 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
 
 from drf_spectacular.utils import extend_schema
 
 from .models import Film
 from .serializers import FilmSerializer
 from .filters import FilmFilter
-
-# @extend_schema(tags=["Films"])
-# class FilmViewSet(ReadOnlyModelViewSet):
-#     queryset = Film.objects.all()
-#     serializer_class = FilmSerializer
-#     permission_classes = [IsAuthenticated]
-#     filter_backends = [FilmFilter]
 
 
 @extend_schema(tags=["Films"])
