38b245234dd7918438d20239c8b450b566445977
Eliud Santander <eliud.santander@gmail.com>
2026-06-09 19:47:11 -0700

    Only Admin Users cas write on films

--- backend/catalog/views.py
+++ backend/catalog/views.py
@@ -1,5 +1,5 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from drf_spectacular.utils import extend_schema
 
@@ -7,10 +7,25 @@ from .models import Film
 from .serializers import FilmSerializer
 from .filters import FilmFilter
 
+# @extend_schema(tags=["Films"])
+# class FilmViewSet(ReadOnlyModelViewSet):
+#     queryset = Film.objects.all()
+#     serializer_class = FilmSerializer
+#     permission_classes = [IsAuthenticated]
+#     filter_backends = [FilmFilter]
+
 
 @extend_schema(tags=["Films"])
-class FilmViewSet(ReadOnlyModelViewSet):
+class FilmViewSet(ModelViewSet):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
-    # permission_classes = [IsAuthenticated]
     filter_backends = [FilmFilter]
+
+    def get_permissions(self):
+        """
+        Allows writing methods only to Admin Users
+        """
+        permission_classes = []
+        if self.action not in ["list", "retrieve"]:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
