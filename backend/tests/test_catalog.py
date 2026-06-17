34688ac5be6dcf7f992fd2207d5113b5898203a4
Eliud Santander <eliud.santander@gmail.com>
2026-06-16 19:37:45 -0700

    Tests updated for films endpoint

--- backend/tests/test_catalog.py
+++ backend/tests/test_catalog.py
@@ -5,23 +5,6 @@ from rest_framework import status
 @pytest.mark.django_db
 class TestFilmEndpoints:
 
-    # --- AUTHENTICATION ---
-    def test_list_films_unauthenticated_fails(self, api_client):
-        response = api_client.get("/api/v1/films/")
-
-        assert response.status_code in [
-            status.HTTP_401_UNAUTHORIZED,
-            status.HTTP_403_FORBIDDEN,
-        ]
-
-    def test_retrieve_film_unauthenticated_fails(self, api_client):
-        response = api_client.get("/api/v1/films/1/")
-
-        assert response.status_code in [
-            status.HTTP_401_UNAUTHORIZED,
-            status.HTTP_403_FORBIDDEN,
-        ]
-
     # --- HAPPY PATHS ---
     def test_list_films_authenticated(self, api_client, test_user):
         api_client.force_authenticate(user=test_user)
